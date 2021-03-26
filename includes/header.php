7b87b61c5a0a2e931415d9edc1cf5a1f6ab65ce6
GyebnarPeter <gyebnarpetter@gmail.com>
2021-03-26 12:17:40 +0100

    basic welcome screen

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="./img/favicon.png"/>
+    <link rel="stylesheet" href="./libs/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400;1,600&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Royal Desszert</title>
+</head>
+<body>
+    <header>
+        <div class="header-logo">
+            <img src="./img/header-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Kézműves torta</a></li>
+                <li><a href="#">Kézműves csokoládé</a></li>
+                <li><a href="#">Macaronok</a></li>
+                <li><a href="#">Rólam</a></li>
+                <li><a href="#">Kapcsolat</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php' ?>
+
+<div class="container">
+    <div class="slider">
+        <div class="slider-page">
+            <div class="slider-content">
+                <div class="slider-cake">
+                    <img src="./img/cake-2.webp" alt="">
+                </div>
+            </div>
+            <div class="slider-content">
+                <div class="slider-text">
+                    <h1>Royal Desszert</h1>
+                    <div class="placeholder"></div>
+                    <p>Kézműves torta, ami felségesen finom, közben gluténmentes és tejfehérje mentes is! Most biztosan kételkedsz egy picit. Igaz?</p>
+                    <a href="#">TOVÁBB</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include 'includes/footer.php' ?>
