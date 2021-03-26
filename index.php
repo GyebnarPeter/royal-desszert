57bf3752f900a879d4078e9a400c6269bc38a6cd
GyebnarPeter <gyebnarpetter@gmail.com>
2021-03-26 16:34:52 +0100

    basic said-about-us section

--- index.php
+++ index.php
@@ -11,12 +11,43 @@
             <div class="slider-content">
                 <div class="slider-text">
                     <h1>Royal Desszert</h1>
-                    <div class="placeholder"></div>
+                    <div class="slider-placeholder"></div>
                     <p>Kézműves torta, ami felségesen finom, közben gluténmentes és tejfehérje mentes is! Most biztosan kételkedsz egy picit. Igaz?</p>
-                    <a href="#">TOVÁBB</a>
+                    <a href="#">tovább</a>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="said-about-us">
+        <div class="said-about-us-header">
+            <h2>Rólunk mondták</h2>
+            <div class="decor-line"></div>
+        </div>
+        <p>5 éve kísérletezem ki a recepteket a saját konyhámban, hogy olyan ízvilágot teremtsek, amire ételallergiás vagy, vagy sem azt mond: <b>„Húúú ez finom!”</b><br>
+           Ahogyan nincs két egyforma ember, vagy igény, vagy kívánság, nincs két egyforma torta sem.
+           A saját egyéniségemet a klasszikus könnyed eleganciát ötvözöm a te kéréseiddel, igényeiddel.<br>
+           Így készül el az a kézműves torta, amire azt mondod:
+        </p>
+        <div class="opinion-boxes">
+            <div class="opinion-box">
+                <img src="./img/cookie-1.png" alt="">
+                <h4>Klaudia</h4>
+                <p>"Ez elképesztő lett!"</p>
+            </div>
+            <div class="opinion-box">
+                <img src="./img/cookie-2.png" alt="">
+                <h4>Lilla</h4>
+                <p>"Épp ilyet álmodtam meg, köszönöm!"</p>
+            </div>
+            <div class="opinion-box">
+                <img src="./img/cookie-3.png" alt="">
+                <h4>Péter</h4>
+                <p>"Ez tényleg jól néz ki!</p>
+            </div>
+        </div>
+        <p>Szeretném hallani a te első mondatodat is!<br>Készen állsz a közös felségesen finom kézműves torta kalandra?</p>
+        <a href="#">igen</a>
     </div>
 
 <?php include 'includes/footer.php' ?>
